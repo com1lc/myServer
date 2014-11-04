f6a1136a8c135553dbdd9c4099024f49ab8870a4
ParrToo <plato2h@gmail.com>
2014-11-04 22:21:37 +0700

    edit git commit pass

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+//app.js
+angular.module("myApp", [])
+.controller('mainCtrl', function($scope, $http){
+	$scope.books = [];
+	$http.get('/api/book').success(function(data){
+		$scope.books = data;
+	})
+
+})
